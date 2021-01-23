33a0407d9fe1111818d8ab883a365b035309c55a
Jonathan Loiseau <37367555+JonathanLoiseau@users.noreply.github.com>
2021-01-23 10:15:25 +0100

    Add files via upload

---
+++ backendbibliorestapi (1)/backendbibliorestapi/src/main/java/com/itjloiseau/backendbibliorestapi/BackendbibliorestapiApplication.java
@@ -0,0 +1,13 @@
+package com.itjloiseau.backendbibliorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbibliorestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendbibliorestapiApplication.class, args);
+	}
+
+}
---
+++ backendbibliorestapi (1)/backendbibliorestapi/src/test/java/com/itjloiseau/backendbibliorestapi/BackendbibliorestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itjloiseau.backendbibliorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbibliorestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
